1df853d43e3dadb2eba44b4a85f2e26aae62e72e
gitlab-runner <do-not-reply@cypress.com>
2026-03-26 19:39:36 -0500

    Upload mtb-example-psoc4-i2c-slave-callback [4724]

--- main.c
+++ main.c
@@ -1,52 +1,51 @@
 /******************************************************************************
-* File Name:   main.c
-*
-* Description: This is the source code for the PSoC 4 MCU I2C Slave Using
-*               Callbacks example for ModusToolbox.
-*
-* Related Document: See README.md 
-*
-*
-*******************************************************************************
-* Copyright 2020-2025, Cypress Semiconductor Corporation (an Infineon company) or
-* an affiliate of Cypress Semiconductor Corporation.  All rights reserved.
-*
-* This software, including source code, documentation and related
-* materials ("Software") is owned by Cypress Semiconductor Corporation
-* or one of its affiliates ("Cypress") and is protected by and subject to
-* worldwide patent protection (United States and foreign),
-* United States copyright laws and international treaty provisions.
-* Therefore, you may use this Software only as provided in the license
-* agreement accompanying the software package from which you
-* obtained this Software ("EULA").
-* If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
-* non-transferable license to copy, modify, and compile the Software
-* source code solely for use in connection with Cypress's
-* integrated circuit products.  Any reproduction, modification, translation,
-* compilation, or representation of this Software except as specified
-* above is prohibited without the express written permission of Cypress.
-*
-* Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
-* EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
-* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
-* reserves the right to make changes to the Software without notice. Cypress
-* does not assume any liability arising out of the application or use of the
-* Software or any product or circuit described in the Software. Cypress does
-* not authorize its products for use in any products where a malfunction or
-* failure of the Cypress product may reasonably be expected to result in
-* significant property damage, injury or death ("High Risk Product"). By
-* including Cypress's product in a High Risk Product, the manufacturer
-* of such system or application assumes all risk of such use and in doing
-* so agrees to indemnify Cypress against all liability.
-*******************************************************************************/
+ * File Name:   main.c
+ *
+ * Description: This is the source code for the PSOC 4 MCU I2C Slave Using
+ *              Callbacks example for ModusToolbox.
+ *
+ * Related Document: See README.md 
+ *
+ *
+ ******************************************************************************
+ * (c) 2020-2026, Infineon Technologies AG, or an affiliate of Infineon
+ * Technologies AG. All rights reserved.
+ * This software, associated documentation and materials ("Software") is
+ * owned by Infineon Technologies AG or one of its affiliates ("Infineon")
+ * and is protected by and subject to worldwide patent protection, worldwide
+ * copyright laws, and international treaty provisions. Therefore, you may use
+ * this Software only as provided in the license agreement accompanying the
+ * software package from which you obtained this Software. If no license
+ * agreement applies, then any use, reproduction, modification, translation, or
+ * compilation of this Software is prohibited without the express written
+ * permission of Infineon.
+ *
+ * Disclaimer: UNLESS OTHERWISE EXPRESSLY AGREED WITH INFINEON, THIS SOFTWARE
+ * IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING, BUT NOT LIMITED TO, ALL WARRANTIES OF NON-INFRINGEMENT OF
+ * THIRD-PARTY RIGHTS AND IMPLIED WARRANTIES SUCH AS WARRANTIES OF FITNESS FOR A
+ * SPECIFIC USE/PURPOSE OR MERCHANTABILITY.
+ * Infineon reserves the right to make changes to the Software without notice.
+ * You are responsible for properly designing, programming, and testing the
+ * functionality and safety of your intended application of the Software, as
+ * well as complying with any legal requirements related to its use. Infineon
+ * does not guarantee that the Software will be free from intrusion, data theft
+ * or loss, or other breaches ("Security Breaches"), and Infineon shall have
+ * no liability arising out of any Security Breaches. Unless otherwise
+ * explicitly approved by Infineon, the Software may not be used in any
+ * application where a failure of the Product or any consequences of the use
+ * thereof can reasonably be expected to result in personal injury.
+ *****************************************************************************/
 
-/* Header file includes */
+/******************************************************************************
+ * Include Header files
+ *****************************************************************************/
 #include "cy_pdl.h"
 #include "cybsp.h"
 
-/*******************************************************************************
-* Macros
-*******************************************************************************/
+/******************************************************************************
+ * Macros
+ *****************************************************************************/
 /* Valid command packet size of three bytes */
 #define PACKET_SIZE          (0x03u)
 
@@ -68,40 +67,36 @@
 #define PACKET_LED_POS       (0x01u)
 #define PACKET_EOP_POS       (0x02u)
 
-
-/*******************************************************************************
-* Function Prototypes
-*******************************************************************************/
+/******************************************************************************
+ * Function Prototypes
+ *****************************************************************************/
 void sI2C_InterruptHandler(void);
 static void ExecuteCommand(void);
 static void SlaveCallback(uint32_t event);
 
-
-/*******************************************************************************
-* Global Variables
-*******************************************************************************/
-/** The instance-specific context structure.
+/******************************************************************************
+ * Global Variables
+ *****************************************************************************/
+/* The instance-specific context structure.
  * It is used by the driver for internal configuration and
  * data keeping for the I2C. Do not modify anything in this structure.
  */
 cy_stc_scb_i2c_context_t CYBSP_I2C_context;
 
 /* CYBSP_I2C_SCB_IRQ */
-const cy_stc_sysint_t CYBSP_I2C_SCB_IRQ_config = {
-        .intrSrc = (IRQn_Type) CYBSP_I2C_IRQ,
-        .intrPriority = 3u
+const cy_stc_sysint_t CYBSP_I2C_SCB_IRQ_config = 
+{
+    .intrSrc = (IRQn_Type) CYBSP_I2C_IRQ,
+    .intrPriority = 3u
 };
 
 /* I2C read and write buffers */
-uint8_t i2cReadBuffer [SL_RD_BUFFER_SIZE] = {PACKET_SOP, STS_CMD_FAIL,
-                                            PACKET_EOP};
+uint8_t i2cReadBuffer [SL_RD_BUFFER_SIZE] = {PACKET_SOP, STS_CMD_FAIL, PACKET_EOP};
 uint8_t i2cWriteBuffer[SL_WR_BUFFER_SIZE];
 
-
-/*******************************************************************************
+/******************************************************************************
  * Function Name: main
- *******************************************************************************
- *
+ ******************************************************************************
  *  The main function performs the following actions:
  *   1. Sets up I2C to be in I2C slave mode.
  *   2. Initializes PWM to control the LED.
@@ -109,14 +104,14 @@ uint8_t i2cWriteBuffer[SL_WR_BUFFER_SIZE];
  *      drive the LED.
  *   4. Slave responds with the acknowledgment packet.
  *
- ******************************************************************************/
+ *****************************************************************************/
 int main(void)
 {
     cy_rslt_t result;
 
     /* Initialize the device and board peripherals */
-    result = cybsp_init() ;
-    if (result != CY_RSLT_SUCCESS)
+    result = cybsp_init();
+    if(result != CY_RSLT_SUCCESS)
     {
         CY_ASSERT(0);
     }
@@ -124,8 +119,7 @@ int main(void)
     /* Initialize and enable I2C Component in slave mode.
      * If initialization fails process error
      */
-    result = Cy_SCB_I2C_Init(CYBSP_I2C_HW, &CYBSP_I2C_config,
-                            &CYBSP_I2C_context);
+    result = Cy_SCB_I2C_Init(CYBSP_I2C_HW, &CYBSP_I2C_config, &CYBSP_I2C_context);
     if(result != CY_SCB_I2C_SUCCESS)
     {
         CY_ASSERT(0);
@@ -171,36 +165,32 @@ int main(void)
 
     for(;;)
     {
-
     }
 }
 
-
-/*******************************************************************************
+/******************************************************************************
  * Function Name: sI2C_InterruptHandler
- *******************************************************************************
+ ******************************************************************************
  * Summary:
  *  This function executes interrupt service routine.
  *
- ******************************************************************************/
+ *****************************************************************************/
 void sI2C_InterruptHandler(void)
 {
     /* ISR implementation for I2C */
     Cy_SCB_I2C_SlaveInterrupt(CYBSP_I2C_HW, &CYBSP_I2C_context);
 }
 
-
-/*******************************************************************************
+/******************************************************************************
  * Function Name: SlaveCallback
- *******************************************************************************
- *
+ ******************************************************************************
  * Summary:
  *  Handles slave events write and read completion events.
  *
  * Parameters:
  *  event:  Reports slave events.
  *
- ******************************************************************************/
+ *****************************************************************************/
 static void SlaveCallback(uint32_t event)
 {
     /* Check write complete event */
@@ -221,7 +211,7 @@ static void SlaveCallback(uint32_t event)
                     /* Execute command and update reply status for received
                      * command
                      */
-                    ExecuteCommand( );
+                    ExecuteCommand();
                     i2cReadBuffer[PACKET_STS_POS] = STS_CMD_DONE;
                 }
             }
@@ -242,20 +232,18 @@ static void SlaveCallback(uint32_t event)
     }
 }
 
-
-/*******************************************************************************
+/******************************************************************************
  * Function Name: ExecuteCommand
- *******************************************************************************
+ ******************************************************************************
+ * Summary:
+ *  Sets the compare value for the LED PWM received by slave.
  *
- * Sets the compare value for the LED PWM received by slave.
- *
- ******************************************************************************/
+ *****************************************************************************/
 static void ExecuteCommand(void)
 {
     /* Sets the compare value to control the brightness of theLED. */
     Cy_TCPWM_PWM_SetCompare0(CYBSP_PWM_HW, CYBSP_PWM_NUM,
                             i2cWriteBuffer[PACKET_LED_POS]);
 }
-
 
 /* [] END OF FILE */
